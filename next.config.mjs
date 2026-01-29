af64509cab50e726cb93d398ce5e7b84c0e2312e
Jon Thia <jonthia@drift.trade>
2026-01-29 13:37:40 +1100

    ft: index landing pages

--- next.config.mjs
+++ next.config.mjs
@@ -9,8 +9,13 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: "/",
+        destination: "/protocol/",
+        permanent: true,
+      },
+      {
         source: "/about-v2/:path*",
-        destination: "/about-v3/:path*",
+        destination: "/protocol/about-v3/:path*",
         permanent: true,
       },
     ];
