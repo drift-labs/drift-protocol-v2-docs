f25b1e12f4144f1a5c6a6cabd26dad905d4869a5
Chester Sim <chestersimdy@gmail.com>
2026-09-09 11:30:40 +0800

    fix: prevent duplicate docs logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -6,29 +6,29 @@ export function Logo() {
         width="26"
         height="26"
         alt="Velocity"
-        className="md:hidden dark:hidden"
+        className="md:!hidden dark:!hidden"
       />
       <img
         src="/assets/velocity-logo-dark.svg"
         width="26"
         height="26"
         alt=""
-        className="hidden md:hidden dark:block"
+        className="hidden md:!hidden dark:!block"
       />
-      <span className="hidden md:block">
+      <span className="hidden md:!block">
         <img
           src="/assets/velocity-text-light.svg"
           width="141"
           height="32"
           alt="Velocity"
-          className="h-[26px] w-auto dark:hidden"
+          className="h-[26px] w-auto dark:!hidden"
         />
         <img
           src="/assets/velocity-text-dark.svg"
           width="141"
           height="32"
           alt=""
-          className="hidden h-[26px] w-auto dark:block"
+          className="hidden h-[26px] w-auto dark:!block"
         />
       </span>
     </>
